fb0deb25606499b508178be1bf06077c4ff5b3f4
NightHunter177 <120390133+NightHunter177@users.noreply.github.com>
2023-02-08 23:23:39 +0300

    hunter

--- mobile.js
+++ mobile.js
@@ -1,4 +1,7 @@
 var counterVal = 0;
 document.querySelector('#click-div').addEventListener('touchstart', function(){
     updateDisplay(++counterVal);
+    function updateDisplay(val) {
+        document.getElementById("counter-label").innerHTML = val;
+    }
 });
